7c8f5a971265f3c0d083921ba899c13efa069c6b
adnanakhlasofficial <official.adnanakhlas@gmail.com>
2025-06-27 23:25:56 +0600

    create: organization dashboard home

---
+++ src/app/(dashboard)/organization/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import OrganizationDashboard from "@/components/organization/dashboard";
+import React from "react";
+
+export default function Page() {
+  return (
+    <div>
+      <OrganizationDashboard />
+    </div>
+  );
+}
